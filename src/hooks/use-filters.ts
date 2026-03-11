43bfab31190c51de051ceacfe303e4382e914140
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-03-11 15:06:45 +0300

    reusable hooks

---
+++ src/hooks/use-filters.ts
@@ -0,0 +1,50 @@
+import {
+	getRouteApi,
+	type RegisteredRouter,
+	type RouteIds,
+	type SearchParamOptions,
+} from "@tanstack/react-router";
+import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "#/lib/constants";
+
+const cleanEmptyParams = <T extends Record<string, unknown>>(search: T) => {
+	const newSearch = { ...search };
+	Object.keys(newSearch).forEach((key) => {
+		const value = newSearch[key];
+		if (
+			value === undefined ||
+			value === "" ||
+			(typeof value === "number" && Number.isNaN(value))
+		)
+			delete newSearch[key];
+	});
+
+	if (search.pageIndex === DEFAULT_PAGE_INDEX) delete newSearch.pageIndex;
+	if (search.pageSize === DEFAULT_PAGE_SIZE) delete newSearch.pageSize;
+
+	return newSearch;
+};
+
+export function useFilters<
+	TId extends RouteIds<RegisteredRouter["routeTree"]>,
+	TSearchParams extends SearchParamOptions<
+		RegisteredRouter,
+		TId,
+		TId
+	>["search"],
+>(routeId: TId) {
+	const routeApi = getRouteApi<TId>(routeId);
+	const navigate = routeApi.useNavigate();
+	const filters = routeApi.useSearch();
+
+	const setFilters = (partialFilters: Partial<TSearchParams>) =>
+		navigate({
+			search: cleanEmptyParams({
+				...filters,
+				...partialFilters,
+			}) as TSearchParams,
+		});
+
+	const resetFilters = () => navigate({ search: {} as TSearchParams });
+
+	return { filters, setFilters, resetFilters };
+}
---
+++ src/hooks/use-form-upsert.tsx
@@ -0,0 +1,91 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "@tanstack/react-router";
+import { toast } from "sonner";
+import type { Route } from "#/lib/constants";
+import type { AppError, Result } from "#/lib/result";
+
+interface UseFormMutationOptions<
+	TData extends { id?: string },
+	TResult = void,
+> {
+	upsertFn: (data: TData) => Promise<Result<TResult>>;
+	entityName: string;
+	navigateTo?: Route["to"];
+	queryKey: string[];
+	successMessage?: {
+		create?: string;
+		update?: string;
+	};
+	onSuccessCallback?: (result: TResult, isEdit: boolean) => void;
+	onErrorCallback?: (error: AppError, isEdit: boolean) => void;
+	displaySuccessToast?: boolean;
+	onReset?: () => void;
+}
+
+export function useFormUpsert<TData extends { id?: string }, TResult = void>({
+	upsertFn,
+	entityName,
+	navigateTo,
+	queryKey,
+	successMessage,
+	onSuccessCallback,
+	displaySuccessToast = true,
+	onErrorCallback,
+	onReset,
+}: UseFormMutationOptions<TData, TResult>) {
+	const queryClient = useQueryClient();
+	const navigate = useNavigate();
+
+	return useMutation({
+		mutationFn: async (data: TData) => {
+			return await upsertFn(data);
+		},
+		onError: (error) => {
+			console.error(":", error);
+			toast.error("Something went wrong", {
+				description: "Unexpected error occured!",
+			});
+		},
+		onSuccess: (result, variables) => {
+			const isEdit = !!variables.id;
+
+			if (!result.success) {
+				onErrorCallback?.(result.error, isEdit);
+				toast.error("Something went wrong", {
+					description: result.error.message,
+				});
+				if (result.error.type === "AuthenticationError") {
+					toast.error("Unauthenticated request!", {
+						description: "You need to be logged in to perform this action!",
+					});
+					navigate({ to: "/login" });
+					return;
+				}
+				toast.error("Something went wrong", {
+					description: result.error.message,
+				});
+				return;
+			}
+
+			const action = isEdit ? "updated" : "created";
+			const defaultMessage = `${entityName} has been ${action} successfully.`;
+			const message = isEdit
+				? successMessage?.update || defaultMessage
+				: successMessage?.create || defaultMessage;
+			if (displaySuccessToast) {
+				toast.success("Success", { description: message });
+			}
+
+			onReset?.();
+
+			queryClient.invalidateQueries({ queryKey });
+			onSuccessCallback?.(result.data, isEdit);
+
+			if (navigateTo) {
+				setTimeout(() => {
+					navigate({ to: navigateTo });
+				}, 0);
+			}
+		},
+	});
+}
