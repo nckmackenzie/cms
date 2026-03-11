1a519a0ce3ec2cdf5dec0e0c2ddae60c06b7c383
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-03-11 16:34:12 +0300

    refactor: change path for locations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,10 +4,10 @@ import { defineConfig } from 'drizzle-kit'
 config({ path: ['.env.local', '.env'] })
 
 export default defineConfig({
-  out: './drizzle',
+  out: './src/db/migrations',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL as string,
   },
 })
