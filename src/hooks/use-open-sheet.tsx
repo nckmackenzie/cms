15d3d8cd96f99ae2358753d76b52b6d132548a97
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-03-24 15:24:09 +0300

    custom hooks

---
+++ src/hooks/use-open-sheet.tsx
@@ -0,0 +1,77 @@
+import { type Route, useNavigate } from "@tanstack/react-router";
+import { useCallback, useEffect } from "react";
+import {
+	type SheetOptions,
+	useSheet,
+} from "#/integrations/providers/sheet-provider";
+
+type SheetMode = "new" | "edit";
+
+type SheetConfig<TSearch> = {
+	// title: string | ((search: TSearch) => string);
+	// description?: string | ((search: TSearch) => string | undefined);
+	options?: Omit<SheetOptions, "onOpenChange">;
+	render: (args: {
+		search: TSearch;
+		closeSheet: () => void;
+	}) => React.ReactNode;
+};
+
+type UseOpenSheetProps<TSearch> = {
+	from: Route["path"];
+	search: TSearch & {
+		sheet?: SheetMode;
+	};
+	configs: Partial<Record<SheetMode, SheetConfig<TSearch>>>;
+};
+
+export function useOpenSheet<TSearch>({
+	from,
+	search,
+	configs,
+}: UseOpenSheetProps<TSearch>) {
+	const navigate = useNavigate({ from });
+	const { setOpen, setClose } = useSheet();
+
+	const closeSheet = useCallback(() => {
+		// navigate({ search: {} });
+		navigate({
+			search: (prev) => {
+				const { sheet, ...rest } = prev as TSearch & { sheet?: SheetMode };
+				return rest;
+			},
+		});
+	}, [navigate]);
+
+	useEffect(() => {
+		const mode = search.sheet;
+
+		if (!mode) {
+			setClose();
+			return;
+		}
+
+		const config = configs[mode];
+
+		if (!config) {
+			setClose();
+			return;
+		}
+
+		const title = config.options?.title;
+
+		const description = config.options?.description;
+
+		setOpen(config.render({ search, closeSheet }), {
+			title,
+			description,
+			onOpenChange: (open) => {
+				if (!open) closeSheet();
+			},
+		});
+
+		return () => {
+			setClose();
+		};
+	}, [search, configs, setOpen, setClose, closeSheet]);
+}
