5a01325ad7b66cd9af9fe4b2be88b9673d09e210
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-03-30 21:39:03 +0300

    remove content collections plugin

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,11 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
-import contentCollections from '@content-collections/vite'
 
 const config = defineConfig({
   plugins: [
     devtools(),
     nitro({ rollupConfig: { external: [/^@sentry\//] } }),
-    contentCollections(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
