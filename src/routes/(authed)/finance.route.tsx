0738f2605b1fb2f61d111a09b99b253de62bf5e0
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-04-15 12:28:31 +0300

    add global `errorComponent and notFoundComponents`

--- src/routes/(authed)/finance.route.tsx
+++ src/routes/(authed)/finance.route.tsx
@@ -1,10 +1,18 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
+import {
+	GlobalNotFound,
+	RouteErrorComponent,
+} from "#/components/ui/route-components";
 
 export const Route = createFileRoute("/(authed)/finance")({
 	component: RouteComponent,
 	staticData: {
 		breadcrumb: "Finance",
 	},
+	errorComponent: ({ error, reset }) => (
+		<RouteErrorComponent error={error} reset={reset} />
+	),
+	notFoundComponent: GlobalNotFound,
 });
 
 function RouteComponent() {
