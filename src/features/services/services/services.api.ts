f8304a03856e278e5f41337dd80f1c5cce02d9ef
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-07-06 13:00:58 +0300

    add function to get service id by `publicId`

--- src/features/services/services/services.api.ts
+++ src/features/services/services/services.api.ts
@@ -2,6 +2,7 @@ import { createServerFn } from "@tanstack/react-start";
 import { and, eq, isNull } from "drizzle-orm";
 import { db } from "#/db";
 import { services } from "#/db/schema";
+import { stringSchema } from "#/lib/schemas";
 import { authMiddleware } from "#/middleware/auth";
 
 export const getCongregationServices = createServerFn()
@@ -24,5 +25,32 @@ export const getCongregationServices = createServerFn()
 						isNull(services.deletedAt),
 					),
 				);
+		},
+	);
+
+export const getServiceByPublicId = createServerFn()
+	.middleware([authMiddleware])
+	.inputValidator(stringSchema("Service Id is required"))
+	.handler(
+		async ({
+			data,
+			context: {
+				user: { congregationId },
+			},
+		}) => {
+			const service = await db.query.services.findFirst({
+				columns: { id: true },
+				where: and(
+					eq(services.congregationId, congregationId),
+					eq(services.publicId, data),
+					isNull(services.deletedAt),
+				),
+			});
+
+			if (!service) {
+				throw new Error("Service not found");
+			}
+
+			return service.id;
 		},
 	);
